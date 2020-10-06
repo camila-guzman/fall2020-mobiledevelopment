61c4d8d66585029a770f63075f6349cfe2d2282e
camilamguzman <>
2020-10-06 16:03:40 -0400

    Update week 6
    Upload new React project

---
+++ week6/src/components/Example/index.js
@@ -0,0 +1,14 @@
+// import React
+import React from 'react';
+
+// class component named Example that either returns or renders a <p> tag
+
+class Example extends React.Component {
+    render() {
+        return (
+            <p>Hi, my name is Camila Guzman.</p>
+        )
+    }
+}
+
+export default Example;
