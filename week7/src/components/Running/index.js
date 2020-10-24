7f82ef9bcc0e9f5b4f00904cb3f0d6ec6bb109b2
camilamguzman <>
2020-10-23 21:40:27 -0400

    Upload Portfolio 1
    Adding two new components and css styles

--- week7/src/components/Running/index.js
+++ week7/src/components/Running/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import DisplayRun from "../DisplayRun";
 
 class Running extends Component {
   constructor(props) {
@@ -9,26 +10,50 @@ class Running extends Component {
   }
 
   render() {
+    let distance = 0.0;
+    let message = "";
+
+    if (this.state.counter > 0) {
+      distance = this.state.counter * 0.25;
+    }
+
+    if (this.state.counter === 40) {
+      message = <p>Please take a break</p>;
+    } else if (this.state.counter === 20) {
+      message = <p>You should take a break soon!</p>;
+    } else if (this.state.counter === 0) {
+      message = "";
+    } else if (this.state.counter % 4 === 0) {
+      message = <p>Nice Work!</p>;
+    }
+
     return (
       <div className="Running" id="run-div">
-        <h4>We're running!</h4>
+        <h2>Mile Tracker</h2>
         <p>Each lap is a 1/4 mile long.</p>
+
         <p>Click the button to keep track of your total laps:</p>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter + 1 });
-          }}
-        >
-          + Lap
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ counter: 0 });
-          }}
-        >
-          Reset
-        </button>
+
+        <div class="buttons-div">
+          <button
+            onClick={() => {
+              this.setState({ counter: this.state.counter + 1 });
+            }}
+          >
+            + Lap
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ counter: 0 });
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        <DisplayRun counter={this.state.counter} />
+        <p class="message">{message}</p>
         <p>Total Laps: {this.state.counter}</p>
+        <p>Total Distance: {distance} Miles</p>
       </div>
     );
   }
--- week7/src/components/Stairs/index.js
+++ week7/src/components/Stairs/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import DisplayStairs from "../DisplayStairs";
 
 class Stairs extends Component {
   constructor(props) {
@@ -9,26 +10,36 @@ class Stairs extends Component {
   }
 
   render() {
+    let steps = 0;
+
+    if (this.state.counter > 0) {
+      steps = this.state.counter * 18;
+    }
     return (
       <div className="Stairs" id="stairs-div">
-        <h4>We're climbing stairs!</h4>
-        <p>Each flight of stairs has 60 steps.</p>
+        <h2>Stair Tracker</h2>
+        <p>Each flight of stairs has 18 steps.</p>
         <p>Click the button to keep track of flights of stairs climbed.</p>
-        <button
-          onClick={() => {
-            this.setState({ counter: this.state.counter + 1 });
-          }}
-        >
-          + Flight of Stairs
-        </button>
-        <button
-          onClick={() => {
-            this.setState({ counter: 0 });
-          }}
-        >
-          Reset
-        </button>
+        <div class="buttons-div">
+          <button
+            onClick={() => {
+              this.setState({ counter: this.state.counter + 1 });
+            }}
+          >
+            + Flight of Stairs
+          </button>
+          <button
+            onClick={() => {
+              this.setState({ counter: 0 });
+            }}
+          >
+            Reset
+          </button>
+        </div>
+        <DisplayStairs counter={this.state.counter} />
+
         <p>Total Flights of Stairs: {this.state.counter}</p>
+        <p>Total Steps: {steps} Steps</p>
       </div>
     );
   }
