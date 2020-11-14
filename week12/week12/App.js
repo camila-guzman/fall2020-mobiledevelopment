d98330c808246d9a24acdbfdd93afbbc21bf0cba
cg-0 <70660599+cg-0@users.noreply.github.com>
2020-11-14 18:47:39 -0500

    Update App.js
    Adding buttons and functions to display what you voted for

--- week12/week12/App.js
+++ week12/week12/App.js
@@ -1,12 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Button, Text, View, YellowBox } from "react-native";
+
+//import ButtonChai from "./components/ButtonChai";
 
 export default function App() {
+  const [selected, setSelected] = useState(false);
+  const [tea, setTea] = useState("tea");
+
+  const displayChaiTea = () => {
+    setSelected(true);
+    setTea("Chai");
+  };
+
+  const displayEarlGreyTea = () => {
+    setSelected(true);
+    setTea("Earl Grey");
+  };
+
+  const displayGreenTea = () => {
+    setSelected(true);
+    setTea("Green");
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {selected ? (
+        <View>
+          <Text style={styles.textTea}>You voted for {tea} tea!</Text>
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.text}>Vote for the best flavor of tea!</Text>
+
+          <Button
+            style={styles.Button}
+            title="Chai"
+            onPress={() => displayChaiTea()}
+          ></Button>
+
+          <Button
+            title="Earl Grey"
+            onPress={() => displayEarlGreyTea()}
+          ></Button>
+
+          <Button title="Green" onPress={() => displayGreenTea()}></Button>
+        </View>
+      )}
     </View>
   );
 }
@@ -14,8 +53,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    marginBottom: "10px",
+  },
+  textTea: {
+    color: "#fff",
+    fontSize: "20px",
+  },
+  Button: {
+    color: "yellow",
   },
 });
