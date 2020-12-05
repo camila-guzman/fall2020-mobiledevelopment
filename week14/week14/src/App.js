9b44f9de179a5cefcb290d394f2de520cbfcd668
cg-0 <70660599+cg-0@users.noreply.github.com>
2020-12-05 00:09:10 -0500

    Update App.js
    Adding components and dispatcher

--- week14/week14/src/App.js
+++ week14/week14/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import { state, reducer } from "./components/AppState";
+
+import IncreaseLaps from "./components/IncreaseLaps";
+import ResetLaps from "./components/ResetLaps";
+import DisplayLaps from "./components/DisplayLaps";
+import GoalReached from "./components/GoalReached";
+import GoalNotReached from "./components/GoalNotReached";
+import DisplayGoal from "./components/DisplayGoal";
+
+import "./App.css";
 
 function App() {
+  // use the useReducer() to create a dispatcher function
+  const [currentState, dispatch] = useReducer(reducer, state);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Track Your Miles!</header>
+      <div className="laps-div">
+        <IncreaseLaps dispatch={dispatch} />
+        <DisplayLaps currentState={currentState} />
+        <ResetLaps dispatch={dispatch} />
+      </div>
+      <div className="goal-div">
+        <GoalReached dispatch={dispatch} />
+        <DisplayGoal currentState={currentState} />
+        <GoalNotReached dispatch={dispatch} />
+      </div>
     </div>
   );
 }
